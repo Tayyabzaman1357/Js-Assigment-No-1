791ec703aafca86ed4cc1bdde411984941a8d283
Tayyabzaman1357 <tayyabzaman807@gmail.com>
2024-10-19 09:30:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Q1: Write a script to greet your website visitors using JS alert box?
+// Ans No 1:   ....................
+ alert("Welcome to our site! We're glad you're here stay tuned for important updates and alerts.");
+
+
+
+
+// Q2:
+// Ans No 2:   .................
+ alert("Error! Please enter a valid password.");
+
+
+
+
+// Q3:
+// Ans No 3:    .......................
+ alert("Welcome to JS Land \n Happy Coding!");
+
+
+
+
+// Q4:
+// Ans No 4:       .................
+ alert("Welcome to JS Land...");
+ alert("happy Coding!");
+
+
+
+
+// Q5:
+// Ans NO 5:   .................
+ console.log("Hello O can run JS through my web browser's console.");
+
+
+
+
+// Q6:
+// Ans No 6:   ..................
+alert("hello!")
